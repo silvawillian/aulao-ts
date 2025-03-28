d11a897623a4e8ed49ed04c0b514bfa846eff42d
Willian Silva <willian.j.silva@gmail.com>
2025-03-27 21:29:52 -0300

    add answers

--- typescript/domain/client.domain.ts
+++ typescript/domain/client.domain.ts
@@ -1,6 +1,32 @@
 export type Person = {
+  id: number;
+  internalNumber: string | number;
+  email: `${string}@${string}.com`;
 };
 
 export class Client {
-  
+  constructor(
+    private id: number,
+    private name: string,
+    private persons: Person[] = []
+  ) {}
+
+  public getId(): number {
+    return this.id;
+  }
+
+  public getName(): string {
+    return this.name;
+  }
+
+  public addPerson(person: Person): void {
+    if (this.persons.includes(person)) {
+      throw new Error(`Person with ID ${person.id} already exists.`);
+    }
+    this.persons.push(person);
+  }
+
+  public listPersons(): Person[] {
+    return this.persons;
+  }
 }
--- typescript/repository/client.database.ts
+++ typescript/repository/client.database.ts
@@ -1,2 +1,19 @@
+import { Client } from "../domain/client.domain";
+import { RepositoryInterface } from "../interfaces/repository.interface";
 
-export class ClientDatabase {}
+export class ClientDatabase implements RepositoryInterface<Client> {
+  private clients: Map<number, Client> = new Map();
+
+  save(client: Client): void {
+    const id = client.getId();
+    this.clients.set(id, client);
+  }
+
+  findById(id: number): Client | null {
+    return this.clients.get(id) || null;
+  }
+
+  all(): Client[] {
+    return Array.from(this.clients.values());
+  }
+}
